7284698b47394e55d163e8ac003e23f70e787b2c
upc <87896540+nsupc@users.noreply.github.com>
2022-10-21 19:43:56 -0400

    see description
    - no need to install dependencies -- uses urllib from standard library
    - better error handling -- won't just break on the first nation it can't log into, will log in console and in error file if a nation is CTE or the password was incorrect
    - user prompted to set user-agent on first time use, can change after in config.json if required

--- login.py
+++ login.py
@@ -1,18 +1,55 @@
-import requests
+import json
 import time
 
-f = open("nations.txt", "r")
-nations = f.read().splitlines()
-f.close
+from datetime import date
+from urllib.error import HTTPError
+from urllib.request import urlopen, Request
 
-user = nations.pop(0)
+headers = {}
+errors = []
 
-for x in nations:
-    nat = x.split(",")
+try:
+    with open("config.json", "r") as in_file:
+        headers["User-Agent"] = f"UPC's login script, being used by {json.load(in_file)['main_nation']}"
 
-    headers = {"User-Agent": user, "X-Password": nat[1]}
+except FileNotFoundError:
+    main_input = str(input("What is your main nation/NS email? "))
 
-    ping = requests.get(
-        "https://www.nationstates.net/cgi-bin/api.cgi?nation={}&q=ping".format(nat[0]), headers=headers)
-    time.sleep(0.6)
-    print(ping)
+    data = {
+        "main_nation": main_input
+    }
+
+    with open("config.json", "w") as out_file:
+        json.dump(data, out_file, indent=4)
+
+    headers["User-Agent"] = f"UPC's login script, being used by {main_input}"
+
+def login_request(nation, password):
+    try:
+        headers["X-Password"] = password
+        request = Request(f"https://www.nationstates.net/cgi-bin/api.cgi?nation={nation.lower().replace(' ', '_')}&q=ping", headers=headers)
+        urlopen(request)
+        print(f"Successfully logged into {nation}")
+    except HTTPError as error:
+        if error.status == 403:
+            print(f"Could not log into {nation} - wrong password")
+            errors.append(f"{nation} - bad password")
+        elif error.status == 404:
+            print(f"Could not log into {nation} - nation does not exist")
+            errors.append(f"{nation} - does not exist")
+    finally:
+        time.sleep(0.6)
+
+try:
+    with open("nations.txt", "r") as in_file:
+        nation_list = in_file.read().split("\n")
+except FileNotFoundError:
+    print("Please create a file called nations.txt formatted like this:\nnation1,password\nnation2,password\netc...")
+else:
+    for item in nation_list:
+        nation, password = item.split(',')
+        login_request(nation, password)
+
+if errors:
+    with open("error.txt", "w") as out_file:
+        out_file.write("{0}\n{1}".format(date.today().strftime('%d/%m/%Y'), '\n'.join(errors)))
